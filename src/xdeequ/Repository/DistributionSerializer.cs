77a78f18d264cb7e4fc75c9d61e09fb46e6d0d8c
Samuele Resca <samuele.resca@gmail.com>
2020-06-21 22:47:29 +0100

    Updating deequ

--- src/xdeequ/Repository/DistributionSerializer.cs
+++ src/xdeequ/Repository/DistributionSerializer.cs
@@ -1,7 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
 namespace xdeequ.Metrics
 {
-    public class DistributionSerializer
+    public class DistributionSerializer : JsonConverter<Distribution>
     {
+        public override Distribution Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            JsonDocument.TryParseValue(ref reader, out var document);
 
+            var values = document.RootElement.GetProperty("values");
+            var distributionValues = values.EnumerateArray().Select(x =>
+               new KeyValuePair<string, DistributionValue>(x.GetProperty("key").GetString(),
+                   new DistributionValue(x.GetProperty("absolute").GetInt64(), x.GetProperty("ratio").GetInt64())));
+
+            return new Distribution(new Dictionary<string, DistributionValue>(distributionValues), document.RootElement.GetProperty("numberOfBins").GetInt64());
+        }
+
+        public override void Write(Utf8JsonWriter writer, Distribution distribution, JsonSerializerOptions options)
+        {
+            writer.WriteNumber("numberOfBins", distribution.NumberOfBins);
+
+            writer.WriteStartArray("values");
+            foreach (var distributionValue in distribution.Values)
+            {
+                writer.WriteString("key", distributionValue.Key);
+                writer.WriteNumber("absolute", distributionValue.Value.Absolute);
+                writer.WriteNumber("ratio", distributionValue.Value.Ratio);
+            }
+
+            writer.WriteEndArray();
+
+        }
     }
 }
--- src/xdeequ/Repository/MetricSerializer.cs
+++ src/xdeequ/Repository/MetricSerializer.cs
@@ -2,19 +2,68 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using xdeequ.Analyzers;
+using xdeequ.Util;
 
 namespace xdeequ.Metrics
 {
-    public class MetricSerializer : JsonConverter<IAnalyzer<IMetric>>
+    public class MetricSerializer : JsonConverter<IMetric>
     {
-        public override IAnalyzer<IMetric> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public override IMetric Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            throw new NotImplementedException();
+            JsonDocument.TryParseValue(ref reader, out var document);
+            var metricName = document.RootElement.GetProperty("metricName").GetString();
+
+            if (metricName == "DoubleMetric")
+            {
+                return new DoubleMetric(
+                    Entity.Column, //.RootElement.GetProperty("entity").GetString();
+                    document.RootElement.GetProperty("name").GetString(),
+                    document.RootElement.GetProperty("instance").GetString(),
+                    document.RootElement.GetProperty("value").GetDouble()
+                    );
+
+            }
+
+            if (metricName == "HistogramMetric")
+            {
+                return new HistogramMetric(
+                    document.RootElement.GetProperty(SerdeExt.COLUMN_FIELD).GetString(),
+                    new Try<Distribution>(
+                        JsonSerializer.Deserialize<Distribution>(document.RootElement.GetProperty("value").GetString())
+                    ));
+            }
+
+            throw new ArgumentException($"Unable to deserialize analyzer {metricName}.");
         }
 
-        public override void Write(Utf8JsonWriter writer, IAnalyzer<IMetric> value, JsonSerializerOptions options)
+        public override void Write(Utf8JsonWriter writer, IMetric value, JsonSerializerOptions options)
         {
-            throw new NotImplementedException();
+            if (value is DoubleMetric dm)
+            {
+                if (!dm.Value.IsSuccess) throw new ArgumentException("Unable to serialize failed metrics.");
+
+                writer.WriteString("metricName", "DoubleMetric");
+                writer.WriteString("entity", dm.Entity.ToString());
+                writer.WriteString("instance", dm.Instance);
+                writer.WriteString("name", dm.Name);
+                writer.WriteString("value", dm.Value.GetOrElse(null).ToString());
+
+                return;
+            }
+
+            if (value is HistogramMetric hm)
+            {
+                if (!hm.Value.IsSuccess) throw new ArgumentException("Unable to serialize failed metrics.");
+
+                writer.WriteString("metricName", "HistogramMetric");
+                writer.WriteString(SerdeExt.COLUMN_FIELD, hm.Column);
+                writer.WriteString("numberOfBins", hm.Value.Get().NumberOfBins.ToString());
+                writer.WriteString("value", JsonSerializer.Serialize(hm.Value.GetOrElse(null)));
+
+                return;
+            }
+
+            //TODO: implement keyed double metric
         }
     }
 }
