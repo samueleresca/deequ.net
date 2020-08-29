310d6aa9b7e81653e31d5efe0c42b49ce3159ba6
Samuele Resca <samuele.resca@gmail.com>
2020-08-29 19:51:14 +0100

    Updating CI

--- examples/examples/ci-scripts/execute-examples-projects.sh
+++ examples/examples/ci-scripts/execute-examples-projects.sh
@@ -5,18 +5,15 @@ PROJECT_NAME=$1
 PROJECT_CONFIG=$2
 DOTNET_SPARK_VERSION=$3
 DOTNET_ALIAS=$4
-
-mkdir -p "$DESTINATION_PATH"
-cd $DESTINATION_PATH
+$SPARK_PATH=$5
 
 echo "PROJECT_NAME ${PROJECT_NAME}"
 echo "PROJECT_CONFIG ${PROJECT_CONFIG}"
 echo "DOTNET_SPARK_VERSION ${DOTNET_SPARK_VERSION}"
 echo "DOTNET_ALIAS ${DOTNET_ALIAS}"
+echo "$SPARK_PATH ${SPARK_PATH}"
 
-cd $DLL_PATH
-
-spark-submit \
+ $SPARK_PATH/bin/spark-submit \
     --class org.apache.spark.deploy.dotnet.DotnetRunner \
     --master local \
     microsoft-spark-2.4.x-$DOTNET_SPARK_VERSION.jar \
