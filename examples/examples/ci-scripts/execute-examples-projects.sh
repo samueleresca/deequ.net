188933ade18452f216ebe7f40308b9a975b255ef
Samuele Resca <samuele.resca@gmail.com>
2020-08-29 20:46:36 +0100

    Fixing SPARK_PATH.

--- examples/examples/ci-scripts/execute-examples-projects.sh
+++ examples/examples/ci-scripts/execute-examples-projects.sh
@@ -8,7 +8,7 @@ PROJECT_NAME=$1
 PROJECT_CONFIG=$2
 DOTNET_SPARK_VERSION=$3
 DOTNET_ALIAS=$4
-$SPARK_PATH=$5
+SPARK_PATH=$5
 
 echo "PROJECT_NAME ${PROJECT_NAME}"
 echo "PROJECT_CONFIG ${PROJECT_CONFIG}"
@@ -16,9 +16,8 @@ echo "DOTNET_SPARK_VERSION ${DOTNET_SPARK_VERSION}"
 echo "DOTNET_ALIAS ${DOTNET_ALIAS}"
 echo "SPARK_PATH ${SPARK_PATH}"
 
-export PATH="${PATH}:${SPARK_PATH}"
 
-exec spark-submit \
+ $SPARK_PATH/spark-submit \
     --class org.apache.spark.deploy.dotnet.DotnetRunner \
     --master local \
     microsoft-spark-2.4.x-$DOTNET_SPARK_VERSION.jar \
